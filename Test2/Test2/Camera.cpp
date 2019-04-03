156184cc043cafced038dc33943d927407ea2d6b
Unknown <b7004011@my.shu.ac.uk>
2019-04-03 20:52:30 +0100

    Starting camera

--- Test2/Test2/Camera.cpp
+++ Test2/Test2/Camera.cpp
@@ -23,8 +23,10 @@ void Camera::Render(FX::MyFX* fx, Vector3 playerPos)
 
 	Vector3 worldPos = Vector3::Transform(position, w);
 
+	Matrix PlayerCamera;
+
 	//Create Camera Matrix
-	CreateViewMatrix(FX::GetViewMatrix(), position, Vector3(playerPos.x, playerPos.y + 0.5f, playerPos.z) , Vector3(0, 1, 0));
+	CreateViewMatrix(PlayerCamera, position, Vector3(playerPos.x, playerPos.y + 0.5f, playerPos.z) , Vector3(0, 1, 0));
 	//Create Projection Matrix to be applied to the Camera
 	CreateProjectionMatrix(FX::GetProjectionMatrix(), MyUtils::Deg2Rad(FOV), GetAspectRatio(), 1, 1000.f);
 }
