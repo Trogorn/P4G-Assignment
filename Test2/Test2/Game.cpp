96d52f8901b6c58de244d94c92573fca4047ca87
Lochlann <b7004011@my.shu.ac.uk>
2019-04-04 06:41:47 +0100

    Formatting

--- Test2/Test2/Game.cpp
+++ Test2/Test2/Game.cpp
@@ -10,17 +10,18 @@ using namespace DirectX::SimpleMath;
 
 Matrix gViewMat1, gViewMat2;
 Vector3 AvoidQuad(float y, float minX, float maxX, float minZ, float maxZ, float minXAvoid, float maxXAvoid, float minZAvoid, float maxZAvoid) {
-	/*AvoidQuad parameters explained
-float y: the actual Y value at which you want to spawn
-float minX, maxX: the minimum and maximum value for X in the TOTAL map quad
-float minZ, maxZ:  the minimum and maximum value for Z in the TOTAL map quad
-float minXAvoid, maxXAvoid: the minimum and maximum value for X in the AVOIDED quad
-float minZAvoid, maxZAvoid:  the minimum and maximum value for Z in the AVOIDED quad
+	/*
+	AvoidQuad parameters explained
+	float y: the actual Y value at which you want to spawn
+	float minX, maxX: the minimum and maximum value for X in the TOTAL map quad
+	float minZ, maxZ:  the minimum and maximum value for Z in the TOTAL map quad
+	float minXAvoid, maxXAvoid: the minimum and maximum value for X in the AVOIDED quad
+	float minZAvoid, maxZAvoid:  the minimum and maximum value for Z in the AVOIDED quad
 
-while loop keeps generating X and Z values until the point is not within the avoided quad
+	while loop keeps generating X and Z values until the point is not within the avoided quad
 
-The vector 3 returned has float y parameter and an X and Z value outside the avoided quad
-*/
+	The vector 3 returned has float y parameter and an X and Z value outside the avoided quad
+	*/
 	
 	float returnX = minXAvoid;
 	float returnZ = minZAvoid;
