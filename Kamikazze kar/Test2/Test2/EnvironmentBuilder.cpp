eff23d3089cf9c71dcb416b7a7cb1a848e9da313
ElliotHunter <b7026503@my.shu.ac.uk>
2019-04-17 15:04:04 +0100

    Started separation into EnvironmentBuilder
    Environment builder is now responsible for the floor only currently. Now to expand to the rest of the environment.

--- Kamikazze kar/Test2/Test2/EnvironmentBuilder.cpp
+++ Kamikazze kar/Test2/Test2/EnvironmentBuilder.cpp
@@ -6,6 +6,20 @@ using namespace DirectX::SimpleMath;
 
 void EnvironmentBuilder::BuildFloor()
 {
+
+	mQuad1 = new Model_Kami();
+
+	mQuad1->Initialise(BuildQuad(mMeshMgr));
+	MaterialExt mat = mQuad1->GetMesh().GetSubMesh(0).material;
+	mat.gfxData.Set(Vector4(0.9f, 0.8f, 0.8f, 0), Vector4(0.9f, 0.8f, 0.8f, 0), Vector4(0.9f, 0.8f, 0.8f, 1));
+	mat.pTextureRV = mFX.mCache.LoadTexture("../building1.dds", true, gd3dDevice);
+	mat.texture = "building1.dds";
+	*mQuad1->GetPosition() = Vector3(0, 0, 0);
+	*mQuad1->GetRotation() = Vector3(0, 0, 0);
+	*mQuad1->GetScale() = Vector3(3, 1, 3);
+	mQuad1->SetOverrideMat(&mat);
+
+
 }
 
 void EnvironmentBuilder::BuildAvoidedFloor()
@@ -26,4 +40,7 @@ void EnvironmentBuilder::BuildObstacles()
 
 void EnvironmentBuilder::RenderEnvironment()
 {
+
+	mFX.Render(*mQuad1, gd3dImmediateContext);
+
 }
